9e2b72a9b1ce6161c23407021199492dd24ec3f0
Nazma1997 <nazma.ist.707@gmail.com>
2022-07-02 14:00:59 +0600

    server

---
+++ Student-Management-System-Server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const  bcrypt = require('bcryptjs');
+const connectDB = require('./db');
+const User = require('./models/User');
+const app = express();
+app.use(express.json());
+
+app.post('/register', async(req, res) => {
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+      return res.status(400).json({message: 'Invalid Data'})
+    }
+    let user = await User.findOne({email});
+    if(user){
+      return res.status(400).json({message: "User Already Exist"});
+    }
+
+    user = new User({name, email, password});
+    
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    user.password = hash;
+
+    await user.save();
+
+    return res.status(201).json({message: 'User Created Successfully', user})
+})
+
+
+
+/**
+ * Connect With MongoDb
+ */
+connectDB('mongodb://localhost:27017/attendance-db')
+.then(() => {
+  console.log('DataBase is Connected')
+  app.listen(5000, () => {
+    console.log('Server is Listening on port 5000');
+})
+})
+.catch(error => {
+     console.log(error)
+})
