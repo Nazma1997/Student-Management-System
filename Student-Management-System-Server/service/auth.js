70bb179e9e7df402113985b37cbcfcfb6e118137
Nazma1997 <nazma.ist.707@gmail.com>
2022-07-03 15:03:57 +0600

    server

---
+++ Student-Management-System-Server/service/auth.js
@@ -0,0 +1,53 @@
+const User = require('../models/User');
+const  bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {findUserByProperty, createNewUser} = require('./user')
+
+const registerService = async({name, email, password}) => {
+
+  let user = await findUserByProperty('email', email);
+  if(user){
+   
+      const error = new Error('User Already Exist');
+      error.status= 400;
+      throw error;
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+ 
+     return  createNewUser({name, email, password: hash});
+    
+      
+};
+
+
+const loginService = async({email, password}) => {
+  const user= await findUserByProperty('email', email);
+
+  if(!user){
+   const error = new Error('Invalid Credential');
+   error.status = 400;
+   throw error;
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch){
+    const error = new Error('Invalid Credential');
+    error.status = 400;
+    throw error;
+  }
+
+    const payload = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      roles: user.roles,
+      accountStatus: user.accountStatus
+    }
+    return jwt.sign(payload, 'secret-key', {expiresIn: '2h'})
+};
+
+module.exports = {
+  registerService,
+  loginService
+}
