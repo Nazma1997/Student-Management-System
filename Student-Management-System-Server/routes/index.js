480498154b33cb12fcf08856762b839a667a3d57
Nazma1997 <nazma.ist.707@gmail.com>
2022-07-07 11:23:40 +0600

    Server

--- Student-Management-System-Server/routes/index.js
+++ Student-Management-System-Server/routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const authRoutes = require('./auth');
 const userRoutes = require('../routes/users');
 const authenticate = require('../middleware/authenticate');
-
+const adminAttendanceRoute = require('./admin-attendance');
 
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/users', authenticate,  userRoutes);
-
+router.use('/api/v1/admin/attendance', authenticate, adminAttendanceRoute )
 
 module.exports = router;
